3531ef51440ac37a63ffe31b730601baae382ca3
Yaniv Ben-Zaken <me@yaniv.bz>
2013-08-22 20:53:21 +0300

    move index statics to subfolder

--- assets.py
+++ assets.py
@@ -4,7 +4,7 @@ from flask.ext.assets import Environment, Bundle
 assets = Environment(app)
 
 vendor_js = Bundle('vendor/lodash/lodash.js', 'vendor/backbone/backbone.js')
-index_coffee = Bundle('index.coffee', filters='coffeescript', output='index.coffee.js')
+index_coffee = Bundle('index/*.coffee', filters='coffeescript', output='gen/index.coffee.js')
 index_js = Bundle(vendor_js, index_coffee, filters='yui_js', output='gen/index.js')
 
 assets.register('index_js', index_js)
